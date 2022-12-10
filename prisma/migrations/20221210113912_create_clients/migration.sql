41070141820cc26df083cee92b902ad42d4b61ca
Natan Fernandes <natangt11@gmail.com>
2022-12-10 08:42:17 -0300

    add the first migrate and creating the basic structure of project

---
+++ prisma/migrations/20221210113912_create_clients/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "clients" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "email" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "clients_pkey" PRIMARY KEY ("id")
+);
