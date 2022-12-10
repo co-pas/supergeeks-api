dbb6415b4b967ce7f584051e40d076ff96aa3e7b
Natan Fernandes <natangt11@gmail.com>
2022-12-10 08:54:30 -0300

    adding error handling

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,22 @@ const server = express();
 
 server.use(express.json());
 
+server.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof Error) {
+      return response.status(400).json({
+        status: "error",
+        message: error.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: "internal server error",
+    });
+  }
+);
+
 server.listen(3000, "localhost", () => {
   console.log("Server started.");
 });
